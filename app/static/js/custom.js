88e3b939d002637d75fcfa60e1af798490f5c3d5
Max Weller <max@teamwiki.de>
2019-07-08 16:37:05 +0200

    asdf

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
                         text = "Good choice! The song will be downloaded.";
                     }
                 }
-                $.jGrowl(text, { life: 4000 });
+                messageBar.show("success", text, 4000, false);
         });
     }
 
@@ -134,5 +134,45 @@ $(document).ready(function() {
            }
         }
     };
+
+    window.messageBar = new MessageBar();
 });
+
+
+//==>
+//==> MessageBar helper
+
+function MessageBar() {
+    this.show = function(className, text, interval, isHtml) {
+      var id = "loadingWidget_" + className;
+      if ($('#'+id).length == 0)
+        $('<div id="'+id+'" class="messageBar"></div>').prependTo("body").click(function(){messageBar.hide(className)});
+      var $el = $('#'+id);
+      if (isHtml) $el.html(text); else $el.text(text);
+      $el.addClass(className).slideDown();
+      if (interval) setInterval(function() { messageBar.hide(className); }, interval);
+    };
+    this.hide = function(className) {
+      $("#loadingWidget_"+className).slideUp();
+    };
+    var loading = $("<div class='progressBar'></div>").prependTo("body").hide();
+    
+    $(document).ajaxStart(function() {
+        loading.show();
+    });
+    $(document).ajaxStop(function() {
+        loading.hide();
+    });
+    $(document).ajaxError(function(event, jqxhr, settings, thrownError) {
+        console.log("ajaxError",event,jqxhr,thrownError);
+        if (event.data) {
+            messageBar.show("error", "Fehler: " + event.data.error, 3000);
+        } else if (jqxhr.status) {
+            messageBar.show("error", "Allgemeiner Fehler: " + jqxhr.status + " " + jqxhr.statusText, 3000);
+        } else {
+            messageBar.show("error", "Exception: " + thrownError, 3000);
+        }
+    });
+    
+};
 
