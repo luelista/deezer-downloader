4742241f0f8147080e233c222db778b8398b4ed5
Max Weller <max@teamwiki.de>
2019-07-08 16:09:36 +0200

    refactor

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -108,34 +108,20 @@ $(document).ready(function() {
         search_album();
     });
 
+    var bbody = document.getElementById('body');
+    bbody.onkeydown = function (event) {
+        if (event.key !== undefined) {
+           if (event.key === 'Enter' && event.altKey) {
+              search_album();
+           }  else if (event.key === 'Enter' ) {
+              search_track();
+           } else if (event.key === 'm' && event.ctrlKey) {
+              $("#query").val("");
+              $("#query").focus();
+           } else if (event.key === 'b' && event.ctrlKey) {
+              window.location = "/";
+           }
+        }
+    };
 });
 
-var bbody = document.getElementById('body');
-bbody.onkeydown = function (event) {
-    if (event.key !== undefined) {
-       if (event.key === 'Enter' && event.altKey) {
-         $.post(deezer_downloader.api_root + '/search', 
-            JSON.stringify({ type: "album", query: $('#query').val() }),
-            function(data) {
-                $("#results > tbody").html("");
-                for (var i = 0; i < data.length; i++) {
-                    drawTableEntry(data[i], "album");
-                }
-         });
-       }  else if (event.key === 'Enter' ) {
-          $.post(deezer_downloader.api_root + '/search', 
-              JSON.stringify({ type: "track", query: $('#query').val() }),
-              function(data) {
-                  $("#results > tbody").html("");
-                  for (var i = 0; i < data.length; i++) {
-                      drawTableEntry(data[i], "track");
-                  }
-          });
-       } else if (event.key === 'm' && event.ctrlKey) {
-          $("#query").val("");
-          $("#query").focus();
-       } else if (event.key === 'b' && event.ctrlKey) {
-          window.location = "/";
-       }
-    }
-};
